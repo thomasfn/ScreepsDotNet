01aefdd90d335f5a9127bacda985b0d5c26f9b78
Thomas Smith <thomasfn@fortfn.co.uk>
2023-11-28 17:30:18 +0000

    Updated example entrypoint to make it easier to switch between world and arena.

--- ScreepsDotNet/Program.cs
+++ ScreepsDotNet/Program.cs
@@ -1,3 +1,7 @@
+// Comment out the appropiate preprocessor directive below to switch the example between arena and world mode!
+#define ARENA
+//#define WORLD
+
 using System;
 using System.Runtime.InteropServices.JavaScript;
 
@@ -10,28 +14,42 @@ namespace ScreepsDotNet
 
     public static partial class Program
     {
+#if WORLD
         private static API.World.IGame? game;
+#endif
+#if ARENA
+        private static API.Arena.IGame? game;
+#endif
         private static ITutorialScript? tutorialScript;
 
-        public static void Main()
+        public static void Main() { }
+
+        [JSExport]
+        internal static void Init()
         {
+#if WORLD
             game = new Native.World.NativeGame();
             tutorialScript = new World.BasicExample(game);
+#endif
+#if ARENA
+            game = new Native.Arena.NativeGame();
+            tutorialScript = new Arena.Tutorial10_FinalTest(game);
+#endif
         }
 
         [JSExport]
         internal static void Loop()
         {
+#if WORLD
             game?.Tick();
+#endif
             tutorialScript?.Loop();
-            if (game is API.World.IGame worldGame)
-            {
-                CheckHeap(worldGame.Cpu.GetHeapStatistics());
-            }
-            else if (game is API.Arena.IGame arenaGame)
-            {
-                CheckHeap(arenaGame.Utils.GetHeapStatistics());
-            }
+#if WORLD
+            CheckHeap(game!.Cpu.GetHeapStatistics());
+#endif
+#if ARENA
+            CheckHeap(game!.Utils.GetHeapStatistics());
+#endif
             LogGCActivity();
         }
 
