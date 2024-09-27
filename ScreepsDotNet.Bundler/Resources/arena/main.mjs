a5757afbf55cdb6fb26138db47e484cf5920391f
Thomas Smith <thomasfn@fortfn.co.uk>
2024-09-27 18:20:44 +0100

    Changed bundler to procure js files and main.js code from msbuild properties instead. Moved bootloader distribution and main.js bootloader logic to API project.

--- ScreepsDotNet.Bundler/Resources/arena/main.mjs
+++ ScreepsDotNet.Bundler/Resources/arena/main.mjs
@@ -1,12 +1,5 @@
-import { getCpuTime } from 'game/utils';
-
-import { Bootloader } from './bootloader';
-import { WasmBytes } from './bundle';
-
-const bootloader = new Bootloader('arena', () => getCpuTime() / 1e+6);
-bootloader.compile(WasmBytes);
-bootloader.start([/*CUSTOM_INIT_EXPORT_NAMES*/]); 
+/*STARTUP*/
 
 export function loop() {
-    bootloader.loop();
+/*LOOP*/
 }
